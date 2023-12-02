b5164adc85ed2636ea88f95e9ebf1b8f0d7243dc
Florin-Gabriel Haja <haja.fgabriel@gmail.com>
2023-12-02 17:54:35 +0200

    Solved 2nd part of first problem

---
+++ 01. Trebuchet/main_2.py
@@ -0,0 +1,31 @@
+import re
+
+f = open("input_2.txt", "r")
+
+sum = 0
+
+digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+RE_DIGITS = re.compile("|".join(digits))
+
+for line in f:
+    first = None
+    last = None
+    line = line.strip()
+    candidate = None
+    for i, c in enumerate(line):
+        if c.isdigit():
+            candidate = int(c)
+        else:
+            mo = RE_DIGITS.match(line, i)
+            if mo:
+                candidate = digits.index(mo.group())
+        
+        if first is None:
+            first = candidate
+        last = candidate
+
+    sum += first * 10 + last
+
+print(sum)
+
+f.close()
