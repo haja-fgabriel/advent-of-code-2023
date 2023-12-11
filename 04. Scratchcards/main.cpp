85d815e94ae29e482c7e3f8354fb487d23b77d6a
Florin-Gabriel Haja <haja.fgabriel@gmail.com>
2023-12-11 23:21:28 +0200

    Solved part 2 of day 4 (deliberately complicated)

--- 04. Scratchcards/main.cpp
+++ 04. Scratchcards/main.cpp
@@ -1,3 +1,8 @@
+// Compile with `g++ --std=c++17 -O2 -Wall main.cpp`
+// Run with `a.exe` (on Windows) or `./a.out` (on UNIX systems)
+// Deliberately made to be complicated and to use modern C++
+
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -9,6 +14,7 @@
 #include <cstring>
 #include <cstddef>
 #include <numeric>
+#include <optional>
 
 
 class Card {
@@ -39,10 +45,15 @@ public:
     }
 
     int getScore() {
+        if (memoScore.has_value()) {
+            return memoScore.value();
+        }
         int result = 0;
+        int count = 0;
         std::unordered_map<int, int> frequencies = luckyMap;
         for (auto& number: numbers) {
             if (frequencies[number] > 0) {
+                count ++;
                 if (result == 0) {
                     result = 1;
                 } else {
@@ -51,16 +62,36 @@ public:
                 frequencies[number] --;
             }
         }
+        memoScore.emplace(result);
+        luckyCount.emplace(count);
         return result;
+    }
+
+    int getLuckyCount() {
+        if (luckyCount.has_value()) {
+            return luckyCount.value();
+        }
+
+        int count = 0;
+        std::unordered_map<int, int> frequencies = luckyMap;
+        for (auto& number: numbers) {
+            if (frequencies[number] > 0) {
+                count ++;
+                frequencies[number] --;
+            }
+        }
+        luckyCount.emplace(count);
+        return count;
     }
 
     Card() {}
     
-
 private:
     std::unordered_map<int, int> luckyMap;
     std::vector<int> numbers;
     std::vector<int> luckyNumbers;
+    std::optional<int> memoScore;
+    std::optional<int> luckyCount;
 
     Card(std::vector<int>& numbers, std::vector<int>& luckyNumbers) {
         this->numbers = std::move(numbers);
@@ -85,32 +116,45 @@ std::istream& operator>>(std::istream& inputFile, Card& c) {
 using CardList = std::vector<Card>;
 
 
-class Solution {
-private:
-    std::ifstream inputFile;
+struct Solution {
+    CardList cards;
+    std::vector<int> scores;
 
-public:
-    Solution(const std::string& filename): inputFile(filename) {}
-
-    ~Solution() {}
-
-    int solve() {
-        Card card;
-        CardList cards;
-
-        while (inputFile >> card) {
-            cards.push_back(card);
-        }
-
-        return std::accumulate(cards.begin(), cards.end(), 0, [](int x, Card& c) {
-            return x + c.getScore();
-        });
+    int solveFirst() {
+        return std::accumulate(scores.begin(), scores.end(), 0);
     }
 
+    int solveSecond() {
+        std::vector<int> copies(scores.size(), 1);
+        const int n = copies.size();
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            const int m = cards[i].getLuckyCount();
+            for (int j = i + 1; j <= i + m && j < n; j++) {
+                copies[j] += copies[i];
+            }
+            result += copies[i];
+        }
+        return result;
+    }
 };
 
+std::istream& operator>>(std::istream& f, Solution& s) {
+    Card card;
+    while (f >> card) {
+        s.cards.push_back(card);
+        s.scores.push_back(card.getScore());
+    }
+    return f;
+}
+
 int main() {
-    Solution s("input.txt");
-    std::cout << "Part 1: " << s.solve() << std::endl;
+    std::ifstream f("input.txt");
+    Solution s;
+
+    f >> s;
+    std::cout << "Part 1: " << s.solveFirst() << std::endl;
+    std::cout << "Part 2: " << s.solveSecond() << std::endl;
     return 0;
 }
